295d6e9cb02ab19b9e98b84e3a642f0289048d8a
LUIS MARIANO GOMEZ MARTINEZ <VY3G00R@telcel.net>
2019-12-17 09:17:53 -0600

    COnfiguracion spring security

---
+++ bin/main/static/assets/scripts/common/mensajes.js
@@ -0,0 +1,4 @@
+//You can add an icon to your alert! There are 4 predefined ones: "warning", "error", "success" and "info".
+function mensaje(mensaje,tipo) {
+	swal("Operación",mensaje, tipo);
+}
--- src/main/java/com/teamdev/opticacheguigo/opticacheguigo/ServletInitializer.java
+++ src/main/java/com/teamdev/opticacheguigo/opticacheguigo/ServletInitializer.java
@@ -1,7 +1,16 @@
 package com.teamdev.opticacheguigo.opticacheguigo;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +18,20 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(OpticacheguigoApplication.class);
 	}
+	
+	  @Override
+	    public void onStartup(ServletContext container) throws ServletException {
+	        WebApplicationContext context = getContext();
+	        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(context));
+	        registration.setLoadOnStartup(1);
+	        registration.addMapping("/*");
+	        super.onStartup(container);
+	    }
+	  
+	  private WebApplicationContext getContext() {
+	        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+	        context.setConfigLocation(OpticacheguigoApplication.class.getName());
+	        return context;
+	    }
 
 }
