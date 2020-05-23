cd35652753517ab8d0f9de08b446580afd5fa922
marianogomezmtz <luismarianoazul@gmail.com>
2020-05-23 13:58:07 -0500

    ELIMINACION DE ARCHIVOS INNCESARIOS

--- src/main/java/com/teamdev/opticacheguigo/opticacheguigo/controller/InventarioController.java
+++ src/main/java/com/teamdev/opticacheguigo/opticacheguigo/controller/InventarioController.java
@@ -1,11 +1,13 @@
 package com.teamdev.opticacheguigo.opticacheguigo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class PrincipalTemplate {
+@RequestMapping("/inventario")
+public class InventarioController {
 	
 	@GetMapping("/template")
 	public ModelAndView main() {
